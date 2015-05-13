353e84924f663afb41d257c6ef1e14e72a6c4d17
Cyril Jacquet <cyril.jacquet@plume-creator.eu>
2015-05-13 22:43:56 +0200

    make the minimap work ; bug: when moving twice the cursor, it moves at the bottom; bug : textedit's scrollbar and minimap not in sync

--- src/plume/gui/writingzone/minimap.py
+++ src/plume/gui/writingzone/minimap.py
@@ -4,10 +4,12 @@ Created on 11 mai 2015
 @author: cyril
 '''
 
-from PyQt5.QtWidgets import QTextEdit, QMenu, QGraphicsView, QGraphicsScene, QLabel
+from PyQt5.QtWidgets import QTextEdit, QMenu, QGraphicsView, QGraphicsScene, QLabel,\
+    QGraphicsItem
 from PyQt5.QtCore import pyqtSlot, Qt, QThread, pyqtSignal, QRectF
 from PyQt5.QtGui import QPixmap, QPalette, QTextBlockFormat
-from PyQt5.Qt import QPainterPath, QImage, QPainter, QPen, QPoint, QRect
+from PyQt5.Qt import QPainterPath, QImage, QPainter, QPen, QPoint, QRect,\
+    QAbstractTextDocumentLayout, QFontMetrics
 from PyQt5.QtOpenGL import QGLWidget
 
 class Minimap(QGraphicsView):
@@ -28,10 +30,17 @@ class Minimap(QGraphicsView):
         self.setViewport(QGLWidget())
         self.setScene(self._scene)
         self.setAlignment(Qt.AlignTop)
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        
+        self.nav_cursor = Cursor()
+        self.nav_cursor.graphics_view  = self
+        self._scene.addItem(self.nav_cursor)
         self._graphic_item_list = []
-        
+        self._pixmap = None
         self._text_edit = None
-        
+        self._abstract_doc_layout = None
+        self._is_cursor_moved = False
         
         self._img_generator = ImageGenerator()
         self._img_generator.image_generated.connect(self._update_with)
@@ -47,14 +56,17 @@ class Minimap(QGraphicsView):
             pass
         self._text_edit = text_edit
         self._text_edit.textChanged.connect(self.update)
-        
-
+        self._scrollbar = self._text_edit.verticalScrollBar()
+        self._scrollbar.valueChanged.connect(self._change_cursor_pos)
     
     @pyqtSlot()
     def update(self):
+        if self._is_cursor_moved:
+            pass   
         self._generate_image(self.text_edit)
         
-       
+    def _change_scrollbar_value(self, value):
+            self._scrollbar.setValue(value)
 
     def resizeEvent(self, event):
         
@@ -96,10 +108,129 @@ class Minimap(QGraphicsView):
         item = self._scene.addPixmap(pixmap)
         item.name = "map"
         item.setPos(0,0)
+        self._pixmap = pixmap
         self._graphic_item_list.append(item)
         
         self.setFixedWidth(pixmap.width())
+        
+        # cursor :
+        self.nav_cursor.setZValue(1)
+        ## width :
+        self.nav_cursor.set_width(pixmap.width())
+        ## height :
+        text_edit_height = self.text_edit.height()
+        doc_height = self.text_edit.document().size().height()
+        minimap_height = self._pixmap.height()   
+        self.nav_cursor.set_minimap_height(minimap_height)
+        ratio = doc_height / text_edit_height
+        cursor_height = minimap_height / ratio
+        self.nav_cursor.set_height(cursor_height)
+        ## position :
+        self._change_cursor_pos()
+    
+    def _change_cursor_pos(self):
+        if self._is_cursor_moved == True:
+            return
+        scroll_value = self._scrollbar.value()
+        scroll_max = self._scrollbar.maximum()
+        available_height_for_cursor = self._pixmap.height() - self.nav_cursor.height()
+        if scroll_max != 0:
+            pos_ratio = scroll_value / scroll_max
+            self.nav_cursor.pos_ratio = pos_ratio
+            self.nav_cursor.setPos(0, available_height_for_cursor * pos_ratio)                
+        else:
+            self.nav_cursor.setPos(0,0)            
+  
+    
+class Cursor(QGraphicsItem):
+    '''
+    Cursor
+    '''
+    cursor_pos_changed = pyqtSignal(int, name='cursor_pos_changed')
+    
+    def __init__(self):
+        super(Cursor, self).__init__()
+        self._width = 1
+        self._height = 1
+        self._text_edit_height = 1
+        self._minimap_height = 1
+        self._doc_height = 1
+        self.pos_ratio = 1
+        self.graphics_view = None
+        self.setCursor(Qt.OpenHandCursor)  
+        self.setFlag(QGraphicsItem.ItemIsMovable, True)
 
+    
+    def set_width(self, width):
+        self.prepareGeometryChange()
+        self._width = width
+        
+    def set_height(self, height):
+        self.prepareGeometryChange()
+        self._height = height
+        
+    def height(self):
+        return self._height
+        
+    def set_text_edit_height(self, height):
+        self._text_edit_height = height
+        
+    def set_doc_height(self, height):
+        self._doc_height = height
+                
+    def set_minimap_height(self, height):
+        self._minimap_height = height
+
+    def paint(self, painter, option, widget):
+        pen = QPen(Qt.red)
+        pen.setWidth(2)
+        painter.setPen(pen)
+        painter.drawRoundedRect(1, 1, self._width - 3, self._height - 3, 5, 5)
+        
+    def boundingRect(self):
+        return QRectF(0, 0, self._width, self._height)
+    
+    def mousePressEvent(self, event):
+        if event.buttons() == Qt.LeftButton:
+            self.setCursor(Qt.ClosedHandCursor)
+            self.graphics_view._is_cursor_moved = True
+        
+        
+    def mouseReleaseEvent(self, event):
+        self.setCursor(Qt.OpenHandCursor)
+        self.graphics_view._is_cursor_moved = False
+        
+    def hoverLeaveEvent(self, event):
+        self.setCursor(Qt.OpenHandCursor)
+        self.graphics_view._is_cursor_moved = False 
+                          
+    def mouseMoveEvent(self, event): 
+        #pos = self.mapFromGlobal(event.globalPos())
+        self.graphics_view._is_cursor_moved = True
+        QGraphicsItem.mouseMoveEvent(self, event)
+        if event.buttons() == Qt.LeftButton:
+            max_y_pos = self._minimap_height -  self.height()
+            # fix vertical
+            self.setX(0)
+            if self.y() < 0:
+                self.setY(0)
+            elif self.y() > max_y_pos and max_y_pos < 0:
+                self.setY(0)
+            elif self.y() > max_y_pos and max_y_pos > 0:
+                self.setY(max_y_pos)
+            
+            
+            #move textedit's scrollbar 
+            elif self.pos_ratio > 0:
+                print("self.y()",self.y())
+                print("self.pos_ratio",self.pos_ratio)
+                print("/",int(self.y() / self.pos_ratio))
+                cursor_ratio = self.y() / self._minimap_height
+                print("cursor_ratio",cursor_ratio)
+                r = self.y() + self.height() * cursor_ratio
+                print("r",r)
+                self.graphics_view._change_scrollbar_value(int(r / self.pos_ratio))
+               
 import hashlib
 
 class DocItem():
@@ -149,8 +280,17 @@ class DocItem():
             pix_paint.drawRect(rect)
             pix_paint.fillRect(rect, pen_color)
 
+        #range of tab:
+        tab_default = 80
         
-
+        text ="m"
+        px = QPixmap(100, 100)
+        p = QPainter(px)
+        fm = p.fontMetrics()
+        width = fm.width(text)
+        p.end()
+        number_chars_in_one_tab = int(tab_default / width)
+        
         p_text = self._prepared_text()
         split_p_text = p_text.split("N")
         
@@ -168,6 +308,10 @@ class DocItem():
             for char in text:
                 if char == "c":
                     draw_char(i, Qt.black)
+                if char == "t":
+                    for _ in range(0,number_chars_in_one_tab):
+                        draw_char(i, Qt.transparent)
+                        i+= 1
                 if char == "_":
                     draw_char(i, Qt.transparent)
                 i += 1
@@ -210,6 +354,8 @@ class DocItem():
         for char in string:
             if i in return_pos_list and i != 0:
                 final = "".join([final,"N"])# line return
+            if char == "\t":
+                final = "".join([final,"t"]) #tab
             if char.isalnum():
                 final = "".join([final,"c"]) #char
             else:
@@ -233,6 +379,7 @@ class ImageGenerator(QThread):
         
         self._old_item_list = {}
         self._old_hash_item_dict = {}
+        self._max_width_found = 30
         
     def set_items(self, item_list):
         self._item_list = item_list
@@ -266,10 +413,11 @@ class ImageGenerator(QThread):
         point = QPoint(0,0)       
         width_list = [pix.width() for pix in pix_list]
         max_width = max(width_list)
+        self._max_width_found = max([max_width, self._max_width_found])
         for pix in pix_list:
 
             last_image = image
-            image = QImage(max_width, image.height() + pix.height(), QImage.Format_ARGB32_Premultiplied )
+            image = QImage(self._max_width_found, image.height() + pix.height(), QImage.Format_ARGB32_Premultiplied )
             pix_paint = QPainter(image)
             image.fill(Qt.white)
             pix_paint.drawImage(0,0, last_image)
