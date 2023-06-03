e98270f906f2b76f2fecc794a1e55bde75d5573a
Cyril Jacquet <cyril.jacquet@mailfence.com>
2023-06-03 20:16:17 +0200

    Add Entity generator

---
+++ generator/entities_generator.py
@@ -0,0 +1,123 @@
+from jinja2 import Environment, FileSystemLoader
+import yaml
+import os
+
+
+def generate_cpp_files(manifest_file):
+    with open(manifest_file, "r") as stream:
+        try:
+            manifest_data = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+            return
+
+    entities_data = manifest_data.get("entities", [])
+    entities_list = entities_data.get("list", [])
+    export = entities_data.get("export", "")
+    header_file = entities_data.get("export_header_file", "")
+    path = entities_data.get("forder_path", ".")
+    entities_list_file = entities_data.get("list_file", "generated_files.txt")
+
+    # Organize entities by name for easier lookup
+    entities_by_name = {entity["name"]: entity for entity in entities_list}
+
+    template_env = Environment(loader=FileSystemLoader("templates"))
+    template = template_env.get_template("entity_template.h.jinja2")
+
+    # Default initialization values
+    default_init_values = {
+        "int": "-1",
+        "float": "0.0f",
+        "double": "0.0",
+        "bool": "false",
+        "QString": "QString()",
+        "QDateTime": "QDateTime::currentDateTime()",
+        "QUuid": "QUuid::createUuid()",
+        "QObject": "nullptr",
+        "QList": "QList<>()",
+    }
+
+    generated_files = []
+
+    for entity in entities_list:
+        name = entity["name"]
+        fields = entity["fields"]
+        parent = entity.get("parent", "QObject")
+        generate = entity.get("generate", True)
+
+        if not generate:
+            continue
+
+        # Get the headers to include based on the field types
+        headers = []
+        for field in fields:
+            field_type = field["type"]
+            if field_type.startswith("QList"):
+                include_header_file = (
+                    field_type.split("<")[1].split(">")[0].strip().lower()
+                )
+                headers.append(f'"{include_header_file}.h"')
+            elif field_type in ["QString", "QDateTime", "QUuid"]:
+                headers.append(f"<{field_type}>")
+        # remove duplicates
+        headers = list(set(headers))
+
+        # If the parent is a custom entity defined in the manifest, include its fields as well
+        parent_fields = (
+            entities_by_name.get(parent, {}).get("fields", [])
+            if parent in entities_by_name
+            else []
+        )
+
+        # only keep the fields from the current entity for initialization
+        fields_init_values = ", ".join(
+            [
+                f"{field['name']}({default_init_values.get(field['type'].split('<')[0], '{}')})"
+                for field in fields
+            ]
+        )
+
+        # use parent fields to initialize parent class in constructor
+        parent_init_values = ", ".join(
+            [
+                f"{field['name']}({default_init_values.get(field['type'].split('<')[0], '{}')})"
+                for field in parent_fields
+            ]
+        )
+
+        rendered_template = template.render(
+            name=name,
+            parent=parent,
+            fields=fields,
+            parent_fields=parent_fields,
+            headers=headers,
+            export=export,
+            header_file=header_file,
+            fields_init_values=fields_init_values,
+            parent_init_values=parent_init_values,
+        )
+
+        output_file = os.path.join(path, f"{name.lower()}.h")
+
+        # Create the directory if it does not exist
+        os.makedirs(os.path.dirname(output_file), exist_ok=True)
+
+        # Add the generated file to the list
+        generated_files.append(output_file)
+
+        with open(output_file, "w") as fh:
+            fh.write(rendered_template)
+            print(f"Successfully wrote file {output_file}")
+
+    # After the loop, write the list of generated files to a file
+    with open(entities_list_file, "w") as fh:
+        for file_path in generated_files:
+            # Convert the file path to be relative to the directory of the entities_list_file
+            relative_path = os.path.relpath(
+                file_path, os.path.dirname(entities_list_file)
+            )
+            fh.write(relative_path + "\n")
+
+
+# Main execution
+generate_cpp_files("manifest.yaml")
---
+++ sources/core/domain_test/entity.h
@@ -0,0 +1,117 @@
+#pragma once
+
+#include "domain_global.h"
+#include <QDateTime>
+#include <QUuid>
+
+
+
+namespace Domain
+{
+
+class SKR_DOMAIN_EXPORT Entity : public QObject
+{
+    Q_OBJECT
+
+    Q_PROPERTY(int id READ id WRITE setId)
+
+    
+    Q_PROPERTY(QUuid uuid READ uuid WRITE setUuid)
+
+    
+    Q_PROPERTY(QDateTime creationDate READ creationDate WRITE setCreationdate)
+
+    
+    Q_PROPERTY(QDateTime updateDate READ updateDate WRITE setUpdatedate)
+
+    
+
+  public:
+    Entity() : QObject(){};
+
+   Entity(   int id,   const QUuid &uuid,   const QDateTime &creationDate,   const QDateTime &updateDate ) 
+        : QObject(), m_id(id), m_uuid(uuid), m_creationDate(creationDate), m_updateDate(updateDate)
+    {
+    }
+
+    Entity(const Entity &other) : QObject(other), m_id(other.m_id), m_uuid(other.m_uuid), m_creationDate(other.m_creationDate), m_updateDate(other.m_updateDate)
+    {
+    }
+
+    Entity &operator=(const Entity &other)
+    {
+        if (this != &other)
+        {
+            QObject::operator=(other);
+            m_id = other.m_id;m_uuid = other.m_uuid;m_creationDate = other.m_creationDate;m_updateDate = other.m_updateDate;
+        }
+        return *this;
+    }
+
+
+    // ------ id : -----
+
+    int id() const
+    {
+        
+        return m_id;
+    }
+
+    void setId( int id)
+    {
+        m_id = id;
+    }
+    
+
+    // ------ uuid : -----
+
+    QUuid uuid() const
+    {
+        
+        return m_uuid;
+    }
+
+    void setUuid( const QUuid &uuid)
+    {
+        m_uuid = uuid;
+    }
+    
+
+    // ------ creationDate : -----
+
+    QDateTime creationDate() const
+    {
+        
+        return m_creationDate;
+    }
+
+    void setCreationdate( const QDateTime &creationDate)
+    {
+        m_creationDate = creationDate;
+    }
+    
+
+    // ------ updateDate : -----
+
+    QDateTime updateDate() const
+    {
+        
+        return m_updateDate;
+    }
+
+    void setUpdatedate( const QDateTime &updateDate)
+    {
+        m_updateDate = updateDate;
+    }
+    
+
+
+  private:
+int m_id;
+    QUuid m_uuid;
+    QDateTime m_creationDate;
+    QDateTime m_updateDate;
+    
+};
+
+} // namespace Domain
