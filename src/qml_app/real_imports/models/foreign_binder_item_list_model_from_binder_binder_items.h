c01fe14d7002ae92e0caba59b5b2fa9982f87da4
Cyril Jacquet <cyril.jacquet@mailfence.com>
2026-01-03 15:54:01 +0100

    fix: correct include statement in generated header file

--- src/qml_app/real_imports/models/foreign_binder_item_list_model_from_binder_binder_items.h
+++ src/qml_app/real_imports/models/foreign_binder_item_list_model_from_binder_binder_items.h
@@ -1,7 +1,7 @@
 // This file was generated automatically by Qleany's generator, edit at your own risk!
 // If you do, be careful to not overwrite it when you run the generator again.
 #pragma once
-#include "binder/foreign_binder_item_list_model_from_binder_binder_items.h"
+#include "binder/binder_item_list_model_from_binder_binder_items.h"
 #include <QQmlEngine>
 
 struct ForeignBinderItemListModelFromBinderBinderItems
